25b949a90f90d2578a34f4b59f2e703ed815cd2a
Marc Servat <marc.servat@sage.com>
2023-01-27 13:45:54 +0100

    display results in a table

--- app.js
+++ app.js
@@ -28,24 +28,45 @@ const getTimeDifferenceInMinutes = (date) => {
   return `${Math.round(diffTime / 60000)}min`;
 }
 
+const getCellContent = (str, max, space = " ") => {
+  if(str.length < max) {
+    const chars = max - str.length;
+    return str + space.repeat(chars);
+  }
+  return str;
+}
+
 const getStringifiedQueue = () => {
   if (!queue.length) { 
     return; 
   }
-  const divider = '-------------------';
-  let response = [divider];
-  queue.forEach(item => response.push(`${getTimeDifferenceInMinutes(item.date)} *${item.name}*`));
-  return [...response, divider].join('\n');
+  const header =
+      "```| " + `${getCellContent("time", 10)}| ${getCellContent("name", 30)}|\n` +
+      getCellContent("", 45, "=");
+  const divider = getCellContent("", 45, "-");
+  let response = [];
+  queue.forEach(item => response.push(`| ${getCellContent(getTimeDifferenceInMinutes(item.date), 10)}| ${getCellContent(item.name, 30)}|\n${divider}`));
+
+  return [header, ...response, "```"].join("\n");
 }
+
+
 
 const getStringifiedLog = () => {
   if (!log.length) { 
     return; 
   }
-  const divider = '-------------------';
-  let response = [divider];
-  log.forEach(item => response.push(`${item.command} - ${item.user}`));
-  return [...response, divider].join('\n');
+  const header =
+      "```| " + `${getCellContent("time", 25)}| ${getCellContent("user", 30)}| ${getCellContent("action", 40)}|\n` +
+      getCellContent("", 102, "=");
+  const divider = getCellContent("", 102, "-");
+  let response = [];
+  log.forEach(item => {
+    const date = item.date.toISOString().split("T")[0];
+    const time = item.date.toISOString().split("T")[1].split(".")[0];
+    response.push(`| ${getCellContent(`${date} - ${time}`, 25)}| ${getCellContent(item.user, 30)}| ${getCellContent(item.command, 40)}|\n${divider}`)
+  });
+  return [header, ...response, "```"].join('\n');
 }
 
 app.event('app_home_opened', ({ event, say }) => {
@@ -56,70 +77,70 @@ app.event('app_mentioned', ({ event, say }) => {
   say(`I'm here, <@${event.user}>!`)
 });
 
-app.command('/queue', async ({ command, ack, respond, say }) => {
+app.command('/queue', async ({ command, ack, respond }) => {
   await ack();
-  await say(command.text);
+  await respond(command.text);
   log.push({ command: command.text, user: command.user_name, date: new Date()});
   const commandParams = command.text.split(' ');
   if(commandParams[1] && !commandParams[1].startsWith('@')) {
-    await say(`Error: Name of entity shoud start with @ symbol`);
+    await respond(`Error: Name of entity shoud start with @ symbol`);
     return;
   }
 
   switch (commandParams[0]) {
     case "help":
-      await say(`*/queue list* - show current queue\n
-*/queue add *@entity* - add your entity to the queue\n
-*/queue move *@entity* *position* - move entity to specific position (first, last, number)\n
-*/queue remove *@entity* - remove your entity from the queue\n
-*/queue log - show log\n
-`);
+      await respond(`
+> *list* - show current queue\n
+> *add @entity* - add your entity to the queue\n
+> *move @entity position* - move entity to specific position (first, last, number)\n
+> *remove @entity* - remove your entity from the queue\n
+> *log* - show log\n
+    `);
       break;
     case "list":
       if (!queue.length) {
-        await say(`queue is empty`);
+        await respond(`queue is empty`);
         return true;
       }
-      await say(getStringifiedQueue());
+      await respond(getStringifiedQueue());
       break;
     case "log":
       if (!log.length) {
-        await say(`log is empty`);
+        await respond(`log is empty`);
         return true;
       }
-      await say(getStringifiedLog());
+      await respond(getStringifiedLog());
       break;
     case "add":
       if (queue.find(item => item.name.toLowerCase() === commandParams[1].toLowerCase())) {
-        await say(`<${commandParams[1]}> is already in the queue`);
+        await respond(`<${commandParams[1]}> is already in the queue`);
         return;
       }
       queue.push({ name: commandParams[1], date: new Date()});
-      await say(`<${commandParams[1]}> added`);
-      await say(getStringifiedQueue());
+      await respond(`<${commandParams[1]}> added`);
+      await respond(getStringifiedQueue())
       break;
     case "remove":
       if (!queue.length) {
-        await say(`queue is empty`);
+        await respond(`queue is empty`);
         return true;
       }
       if (queue.find(item => item.name.toLowerCase() === commandParams[1].toLowerCase())) {
         queue = queue.filter(item => item.name.toLowerCase() !== commandParams[1].toLowerCase());
-        await say(`<${commandParams[1]}> removed`);
-        await say(`<${queue[0].name}> is in the first position of queue`);
+        await respond(`<${commandParams[1]}> removed`);
+        await respond(`<${queue[0].name}> is in the first position of queue`);
       } else {
-        await say(`failed, entity *${commandParams[1]}* not found in queue`);
+        await respond(`failed, entity *${commandParams[1]}* not found in queue`);
       }
-      await say(getStringifiedQueue());
+      await respond(getStringifiedQueue());
       break;
     case "move":
-      console.log(parseInt(Number(commandParams[2]), 10), queue.length);
       if (commandParams[2] !== "first" && commandParams[2] !== "last" && (parseInt(Number(commandParams[2]), 10) < 1 || parseInt(Number(commandParams[2]), 10) > queue.length)) {
-        await say(`the second parameter should be "first", "last" or position number between 1 and ${queue.length}`);
+        await respond(`the second parameter should be "first", "last" or position number between 1 and ${queue.length}`);
         return true;
       }
       if (!queue.length) {
-        await say(`can't move entity, queue is empty`);
+        await respond(`can't move entity, queue is empty`);
         return true;
       }
       if (queue.find(item => item.name.toLowerCase() === commandParams[1].toLowerCase())) {
@@ -138,13 +159,13 @@ app.command('/queue', async ({ command, ack, respond, say }) => {
             queue.splice(position, 0, queueEntry);
             break;
         }
-        await say(`<${commandParams[1]}> moved`);
+        await respond(`<${commandParams[1]}> moved`);
       } else {
-        await say(`failed, entity *${commandParams[1]}* not found in queue`);
+        await respond(`failed, entity *${commandParams[1]}* not found in queue`);
       }
-      await say(getStringifiedQueue());
+      await respond(getStringifiedQueue());
       break;
     default:
-      await say(`:red_circle:  *${command.text}* is not a valid option `);
+      await respond(`:red_circle:  *${command.text}* is not a valid option `);
   }
 });
